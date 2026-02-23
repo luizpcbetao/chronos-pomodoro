6a8ca4b73dc11107bdb0a7137b2408cc0c2c5160
Luiz Augusto <luizminorelli27@gmail.com>
2026-02-23 13:14:51 -0300

    Aula 18 - Finalização do componente Heading e tipagem TypeScript de children

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2='String'>
+      <Heading>
         Olá Mundo 1
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        quidem autem ea voluptas illum minus vitae ratione animi tempore
-        reprehenderit, unde dolor accusantium rerum fuga officiis sequi vel id
-        debitis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quidem autem ea voluptas illum minus vitae
+        ratione animi tempore reprehenderit, unde dolor accusantium rerum fuga officiis sequi vel id debitis?
       </p>
     </>
   );
