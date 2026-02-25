cd9aedbe6c5a81ee8a119255295d50b6c1ed0a18
Luiz Augusto <luizminorelli27@gmail.com>
2026-02-25 13:52:19 -0300

    Aula 27 - Input: um componente um pouco mais complexo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -25,8 +26,7 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <label htmlFor='meuInput'>Task</label>
-            <input id='meuInput' type='text' />
+            <DefaultInput id='meuInput' type='text' />
           </div>
 
           <div className='formRow'>
