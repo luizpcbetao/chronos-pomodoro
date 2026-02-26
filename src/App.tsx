265e8df02b3f774fe8f67461b42fdd232418f050
Luiz Augusto <luizminorelli27@gmail.com>
2026-02-26 14:04:35 -0300

    Aula 28 - Input: Usando condicionais no JSX + '...props' (O restante das propriedades)

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' />
+            <DefaultInput labelText='Task' id='meuInput' type='text' />
           </div>
 
           <div className='formRow'>
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,12 +1,21 @@
 type DefaultInputProps = {
   id: string;
+  labelText: string;
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id, type }: DefaultInputProps) {
+export function DefaultInput({ id, type, labelText, ...rest }: DefaultInputProps) {
   return (
     <>
-      <label htmlFor={id}>Task</label>
-      <input id={id} type={type} />
+      {labelText && <label htmlFor={id}>{labelText}</label>}
+      {/* Aqui eu deixo um IF apenas, sem o else */}
+
+      {/* {labelText ? <label htmlFor={id}>{labelText}</label> : ''}  
+      Aqui eu deixo o inspecionar com ou sem dados, a partir dos : é if, else */}
+
+      {/* <label htmlFor={id}>{labelText}</label>  
+      Essa seria a função inicial*/}
+
+      <input id={id} type={type} {...rest} />
     </>
   );
 }
